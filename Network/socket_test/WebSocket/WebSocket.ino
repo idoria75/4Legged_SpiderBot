dc9f0e75b749e45c52264c6ace8edd3cbc9f6672
idoria75 <ivanp_doria@hotmail.com>
2018-10-08 16:58:15 -0300

    Add comments

--- Network/socket_test/WebSocket/WebSocket.ino
+++ Network/socket_test/WebSocket/WebSocket.ino
@@ -1,63 +1,68 @@
+/*
+Developed by Ivan Posca Doria (ivanpdoria@gmail.com)
+This code is used to implement a Websocket communication between
+the ESP32 and a python client (tested on LINUX).
+
+This code was based on the example provided by TechTutorialsX
+https://techtutorialsx.com/2017/11/03/esp32-arduino-websocket-server-over-soft-ap/
+*/
+
 #include <WiFi.h>
 #include <WebSocketServer.h>
- 
+
+// Initialize WebSocketServer
 WiFiServer server(80);
 WebSocketServer webSocketServer;
- 
+
+// Define AP info 
 const char* ssid = "SPIDERBOT";
 const char* password = "seashell";
 
 void setup() {
  
-  Serial.begin(115200);
- 
-  //WiFi.begin(ssid, password);
-  
-  Serial.println("Setting AP (Access Point)…");
+    Serial.begin(115200);
 
-  WiFi.softAP(ssid);
-//  
-//  while (WiFi.status() != WL_CONNECTED) {
-//    delay(1000);
-//    Serial.println("Connecting to WiFi..");
-//  }
- 
-//  Serial.println("Connected to the WiFi network");
-//  Serial.println(WiFi.localIP());
-//
-  Serial.println("Getting IP");
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
-  
-  server.begin();
-  delay(100);
+    Serial.println("Setting up ESP32 as Access Point");
+
+    // Initialize ESP32 in AP Mode
+    WiFi.softAP(ssid);
+
+    // Get IP address of ESP32 in its own network
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+    
+    server.begin();
+    delay(100);
 }
  
 void loop() {
+
+    // Get data from client, if connected
+    WiFiClient client = server.available();
+    
+    // If client connected and handshake is successfull
+    if (client.connected() && webSocketServer.handshake(client)) {
  
-  WiFiClient client = server.available();
+        String data;      
  
-  if (client.connected() && webSocketServer.handshake(client)) {
+        while (client.connected()) {
  
-    String data;      
+            data = webSocketServer.getData();
  
-    while (client.connected()) {
+            // If data
+            if (data.length() > 0) {
+                 Serial.println(data);
+                 data = "Reply: '"+data+"'";
+                 webSocketServer.sendData(data);
+            }
  
-      data = webSocketServer.getData();
+            delay(10); // Delay needed for receiving the data correctly
+     }
  
-      if (data.length() > 0) {
-         Serial.println(data);
-         data = "Reply: '"+data+"'";
-         webSocketServer.sendData(data);
-      }
+     Serial.println("The client disconnected");
+     delay(100);
+    }
  
-      delay(10); // Delay needed for receiving the data correctly
-   }
- 
-   Serial.println("The client disconnected");
-   delay(100);
-  }
- 
-  delay(100);
+    delay(100);
 }
--- Network/socket_test/WebSocket/websocket_to_esp32.py
+++ Network/socket_test/WebSocket/websocket_to_esp32.py
@@ -1,6 +1,15 @@
+# Developed by Ivan Posca Doria (ivanpdoria@gmail.com)
+# This code is used to implement a Websocket communication between
+# the ESP32 and a python client (tested on LINUX).
+
+# This code was based on the example provided by TechTutorialsX
+# https://techtutorialsx.com/2017/11/03/esp32-arduino-websocket-server-over-soft-ap/
+
 import websocket
 import time
- 
+
+time_between_messages = 10
+# Define websocket and connect to server
 ws = websocket.WebSocket()
 ws.connect("ws://192.168.4.1/")
  
@@ -8,10 +17,12 @@ i = 0
 nrOfMessages = 200
  
 while i<nrOfMessages:
-    ws.send("message nr: " + str(i))
+	# Send message
+    ws.send("Message Number: " + str(i))
+    # Gets reply from server
     result = ws.recv()
     print(result)
     i=i+1
-    time.sleep(1)
+    time.sleep(time_between_messages)
  
 ws.close()
