a1611237a126b9d2b5ec9efb850fc6a30d94df7a
idoria75 <ivanp_doria@hotmail.com>
2018-10-08 20:36:13 -0300

    Add 2nd thread to blink LED

--- Threads/Threads.ino
+++ Threads/Threads.ino
@@ -3,15 +3,22 @@
 #include <Thread.h>
 #include <ThreadController.h>
 
-Thread thread1;
+Thread thread1, thread2;
 ThreadController groupOfThreads = ThreadController();
 
-bool led_status = false;
+bool led_status1 = false;
+bool led_status2 = false;
 
+void change_LED_state1(){
+    led_status1 = !led_status1;
+    Serial.println(led_status1);
+    Serial.print("LED:"); Serial.println(LED_BUILTIN);
+}
 
-void change_LED_state(){
-    led_status = !led_status;
-    Serial.println(led_status);
+void change_LED_state2(){
+    led_status2 = !led_status2;
+    Serial.println(led_status2);
+    Serial.print("LED:"); Serial.println("D5");
 }
 
 void setup() {
@@ -19,14 +26,22 @@ void setup() {
     Serial.begin(115200);
 
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(4, OUTPUT);
 
     thread1.onRun([]() {
-        change_LED_state();
+        change_LED_state1();
     });
-    
     thread1.setInterval(500);
 
+    thread2.onRun([](){
+        change_LED_state2();
+    });
+    thread2.setInterval(333);
+
+    Serial.print("LED built-in:"); Serial.println(LED_BUILTIN);
+
     groupOfThreads.add(&thread1);
+    groupOfThreads.add(&thread2);
     //digitalWrite(LED_BUILTIN, led_status);
 }
 
@@ -34,7 +49,8 @@ void loop() {
 
     groupOfThreads.run();
     
-    digitalWrite(LED_BUILTIN, led_status);
+    digitalWrite(LED_BUILTIN, led_status1);
+    digitalWrite(4, led_status2);
     
     yield();
 }
