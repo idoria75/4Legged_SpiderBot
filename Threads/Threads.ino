83d4a7a4e657d997c87fb1af3edc876fc8467225
idoria75 <ivanp_doria@hotmail.com>
2018-10-09 13:53:14 -0300

    Base code with AP and Blink Thread

--- Threads/Threads.ino
+++ Threads/Threads.ino
@@ -1,47 +1,64 @@
-// Based on provant github
+/*
+Developed by Ivan Posca Doria (ivanpdoria@gmail.com)
+*/
 
+#include <WiFi.h>
+#include <WebSocketServer.h>
 #include <Thread.h>
 #include <ThreadController.h>
 
-Thread thread1, thread2;
+// Initialize WebSocketServer
+WiFiServer server(80);
+WebSocketServer webSocketServer;
+
+// Define AP info 
+const char* ssid = "SPIDERBOT";
+const char* password = "seashell";
+
+// Defines threads and their controller:
+Thread thread1, thread2, t_websocket;
 ThreadController groupOfThreads = ThreadController();
 
 bool led_status1 = false;
-bool led_status2 = false;
 
+// Thread1: Blink
 void change_LED_state1(){
     led_status1 = !led_status1;
     Serial.println(led_status1);
     Serial.print("LED:"); Serial.println(LED_BUILTIN);
 }
 
-void change_LED_state2(){
-    led_status2 = !led_status2;
-    Serial.println(led_status2);
-    Serial.print("LED:"); Serial.println("D5");
-}
-
 void setup() {
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-
+    // Pin Setup
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(4, OUTPUT);
+
+    // Initialize serial port
+    Serial.begin(115200);
+
+    // Initialize WiFi service as Access Point (AP)
+    Serial.println("Setting up ESP32 as Access Point");
+    WiFi.softAP(ssid);
+    // Get IP address of ESP32 in its own network
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+    // Start websocket server
+    server.begin();
 
     thread1.onRun([]() {
         change_LED_state1();
     });
     thread1.setInterval(500);
 
-    thread2.onRun([](){
-        change_LED_state2();
+    t_websocket.onRun([]() {
+        Serial.println("Hello Thread");
     });
-    thread2.setInterval(333);
+    t_websocket.setInterval(1000);
 
-    Serial.print("LED built-in:"); Serial.println(LED_BUILTIN);
 
     groupOfThreads.add(&thread1);
-    groupOfThreads.add(&thread2);
+    groupOfThreads.add(&t_websocket);
     //digitalWrite(LED_BUILTIN, led_status);
 }
 
@@ -50,7 +67,6 @@ void loop() {
     groupOfThreads.run();
     
     digitalWrite(LED_BUILTIN, led_status1);
-    digitalWrite(4, led_status2);
     
     yield();
 }
