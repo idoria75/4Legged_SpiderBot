069938dd37874d89ffa15bfdda17b1f3396abbc8
idoria75 <ivanp_doria@hotmail.com>
2018-10-15 18:54:24 -0300

    Movimentation FSM with LEDs

--- Threads/Threads.ino
+++ Threads/Threads.ino
@@ -68,11 +68,11 @@ void receiveDataFromWS(){
 // State #0: SetUp
 // State #1: Reset Stance
 // State #2: Initial Robot Configuration
-// State #3: Move B Forward
-// State #4: Move A Forward
+// State #3: Move B Forward (04)
+// State #4: Move A Forward (15)
 // State #5: Push Forward
-// State #6: Move C Forward
-// State #7: Move D Forward
+// State #6: Move C Forward (26)
+// State #7: Move D Forward (13)
 // State #8: Push Forward -> Loop back to State #3 
 int state = 0;
 int led_4  = 0;
@@ -82,38 +82,78 @@ int led_26 = 0;
 
 void updateStateMachine(){
     switch (state) {
-        case 0:
-            led_15 = 0;
+        case 0: // State #0: SetUp
             led_4  = 1;
-            break;
-        case 1:
-            led_4  = 0;
+            led_13 = 0;
+            led_15 = 0;
             led_26 = 1;
+            state = state + 1;
             break;
-        case 2:
-            led_26 = 0;
+        case 1: // State #1: Reset Stance
+            led_4  = 0;
             led_13 = 1;
+            led_15 = 1;
+            led_26 = 0;
+            state = state + 1;
             break;
-        case 3:
+        case 2: // State #2: Initial Robot Configuration
+            led_4  = 1;
+            led_13 = 1;
+            led_15 = 1;
+            led_26 = 1;
+            state = state + 1;
+            break;
+        case 3: // State #3: Move B Forward (04)
+            led_4  = 1;
+            led_13 = 0;
+            led_15 = 0;
+            led_26 = 0;
+            state = state + 1;
+            break;
+        case 4: // State #4: Move A Forward (15)
+            led_4  = 0;
             led_13 = 0;
             led_15 = 1;
+            led_26 = 0;
+            state = state + 1;
+            break;
+        case 5: // State #5: Push Forward
+            led_4  = 1;
+            led_13 = 1;
+            led_15 = 1;
+            led_26 = 1;
+            state = state + 1;
+            break;
+        case 6: // State #6: Move C Forward (26)
+            led_4  = 0;
+            led_13 = 0;
+            led_15 = 0;
+            led_26 = 1;
+            state = state + 1;
+            break;
+        case 7: // State #7: Move D Forward (13)
+            led_4  = 0;
+            led_13 = 1;
+            led_15 = 0;
+            led_26 = 0;
+            state = state + 1;
+            break;
+        case 8: // State #8: Push Forward -> Loop back to State #3
+            led_4  = 1;
+            led_13 = 1;
+            led_15 = 1;
+            led_26 = 1;
+            state = 3;
             break;
     }
 }
 
 void runStateMachine(){
     updateStateMachine();
-    if(state == 3){
-        state = 0;
-    }
-    else{
-        state = state+1;
-    }
     digitalWrite( 4, led_4 );
     digitalWrite(13, led_13);
     digitalWrite(15, led_15);
     digitalWrite(26, led_26);
-    //delay(100);
 }
 
 void setup() {
