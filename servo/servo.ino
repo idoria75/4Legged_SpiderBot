5ad2c1df3b6d2d780bf7f0a52b455dd2a067f1c0
Ivan Posca Doria <ivanpdoria@gmail.com>
2018-10-23 15:07:37 -0300

    Make spider keep standing up

--- servo/servo.ino
+++ servo/servo.ino
@@ -37,11 +37,11 @@ Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 //#define MAX_ANGLE 100
 //#define MIN_ANGLE 80
 
-#define MAX_ANGLE 120
-#define MIN_ANGLE 60
+#define MAX_ANGLE 105
+#define MIN_ANGLE 75
 
 // our servo # counter
-uint8_t servonum = 0;
+uint8_t servonum = 10;
 
 int new_max = 600;
 int new_min = 150;
@@ -62,7 +62,7 @@ void setup() {
   Serial.println(new_min);
   Serial.print("new_max: ");
   Serial.println(new_max);
-  delay(2000);
+  delay(1000);
 
   // Assumindo frente como a "boca"
   // Pernas 1, 2, 3 e 4
@@ -71,57 +71,65 @@ void setup() {
   // 3 4
 
   int perna1 = 1;
-  int perna2 = 0;
-  int perna3 = 0;
-  int perna4 = 0;
+  int perna2 = 1;
+  int perna3 = 1;
+  int perna4 = 1;
 
   // Perna 1:
   if (perna1 == 1) {
-    pwm.setPWM(0, 0, 400);
-    pwm.setPWM(1, 0, 420);
-    pwm.setPWM(11, 0, 400);
+    pwm.setPWM(00, 0, 400);  // F
+    pwm.setPWM(01, 0, 400);  // F
+    pwm.setPWM(11, 0, 350);  // F
   }
 
   // Perna 2:
   if (perna2 == 1) {
-    pwm.setPWM(2, 0, 350);
-    pwm.setPWM(3, 0, 450);
-    pwm.setPWM(4, 0, 400);
+    pwm.setPWM(2, 0, 350);  // F
+    pwm.setPWM(3, 0, 450);  // F
+    pwm.setPWM(4, 0, 410);  // F
   }
 
-  // Perna 3: (Feita)
+  // Perna 3:
   if (perna3 == 1) {
-    pwm.setPWM(5, 0, 400);
-    pwm.setPWM(6, 0, 350);  // F
-    pwm.setPWM(7, 0, 400);
+    pwm.setPWM(5, 0, 400);  // F
+    pwm.setPWM(6, 0, 410);  // F
+    // pwm.setPWM(7, 0, 300);
   }
 
   // Perna 4:
   if (perna4 == 1) {
-    pwm.setPWM(8, 0, 300);
-    pwm.setPWM(9, 0, 420);
-    pwm.setPWM(10, 0, 400);
+    pwm.setPWM(8, 0, 250);   // F
+    pwm.setPWM(9, 0, 390);   // F
+    pwm.setPWM(10, 0, 400);  // F
   }
 
-  delay(5000);
+  delay(1000);
 }
 
 void loop() {
   // Drive each servo one at a time
+  Serial.print("Servonum: ");
   Serial.println(servonum);
   delay(1000);
+
   // for (uint16_t pulselen = new_min; pulselen < new_max; pulselen++) {
   //   Serial.println(pulselen);
   //   pwm.setPWM(servonum, 0, pulselen);
+  //   delay(10);
   // }
 
   // delay(1000);
   // for (uint16_t pulselen = new_max; pulselen > new_min; pulselen--) {
   //   Serial.println(pulselen);
   //   pwm.setPWM(servonum, 0, pulselen);
+  //   delay(10);
   // }
   // delay(1000);
 
-  // if (servonum > 1)
-  //   servonum = 1;
+  // // servonum++;
+  // if (servonum == 7) {
+  //   servonum = 8;
+  // }
+  // if (servonum > 11)
+  //   servonum = 0;
 }
