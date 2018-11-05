8a7d454cd55712c7daf2f6620139e7b081e09614
Ivan Posca Doria <ivanpdoria@gmail.com>
2018-11-05 20:24:46 -0200

    Define robot as global

--- Threads/src/Threads.ino
+++ Threads/src/Threads.ino
@@ -4,13 +4,15 @@ Developed by Ivan Posca Doria (ivanpdoria@gmail.com)
 
 #include "Definitions.h"
 
+// Rever necessidade de alocar memoria para o robo dinamicamente
+Leg leg1(5, 3, 2, 90, 40, 50);
+Leg leg2(2, 4, 4, 20, 50, 30);
+Leg leg3(6, 5, 3, 10, 70, 40);
+Leg leg4(3, 2, 1, 80, 90, 90);
+Robot rob(leg1, leg2, leg3, leg4);
+
 void setup() {
   run_setUp();
-  Leg leg1(5, 3, 2, 90, 40, 50);
-  Leg leg2(2, 4, 4, 20, 50, 30);
-  Leg leg3(6, 5, 3, 10, 70, 40);
-  Leg leg4(3, 2, 1, 80, 90, 90);
-  Robot rob(leg1, leg2, leg3, leg4);
   rob.print_leg_status();
   rob.update_leg(1, 45, 45, 45);
   Serial.println("Leg A Updated!");
