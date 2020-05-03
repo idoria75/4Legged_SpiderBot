e5549ecb015c92db4cfdbd3beeefd3a80fd9bfbc
Ivan Posca Doria <ivanpdoria@gmail.com>
2020-05-03 08:01:30 +0200

    Remove quadpod-ui due to security issues

--- quadpod-ui/app/store/configureStore.prod.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import { createStore } from 'redux';
-import rootReducer from '../modules/reducers';
-
-function configureStore(initialState) {
-  return createStore(rootReducer, initialState);
-}
-
-export default { configureStore };
